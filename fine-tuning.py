8624ca72405a599c83b265a8b4b940efa65f0673
dirgafebrian <fdirga63@gmail.com>
2024-06-27 16:40:08 +0700

    training model (2)

--- fine-tuning.py
+++ fine-tuning.py
@@ -4,10 +4,10 @@ import torch
 from utils import QADataset
 
 # Load the dataset
-df = pd.read_csv('datasets/1-6.csv', delimiter='|', names=['question', 'answer'])
+df = pd.read_csv('datasets/clean.csv', delimiter='|', names=['question', 'answer'])
 
 # Prepare the dataset
-tokenizer = GPT2Tokenizer.from_pretrained('fine_tuned_gpt2_model')
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2_model')
 tokenizer.pad_token = tokenizer.eos_token
 
 # Combine question and answer into a single string for training
@@ -16,16 +16,16 @@ inputs = df['question'] + tokenizer.eos_token + df['answer']
 dataset = QADataset(inputs, tokenizer)
 
 # Load model
-model = GPT2LMHeadModel.from_pretrained('fine_tuned_gpt2_model')
+model = GPT2LMHeadModel.from_pretrained('gpt2_model')
 
 # Define training arguments for fine-tuning
 fine_tuning_args = TrainingArguments(
-    output_dir='./fine_tuning_results2',
+    output_dir='./fine_tuning_results',
     num_train_epochs=5,                  # Meningkatkan jumlah epoch
     per_device_train_batch_size=8,       # Meningkatkan batch size
     warmup_steps=500,                    # Meningkatkan warmup steps
     weight_decay=0.01,                   # Menyesuaikan weight decay
-    logging_dir='./fine_tuning_logs2',
+    logging_dir='./fine_tuning_logs',
     learning_rate=5e-5,                  # Menyesuaikan learning rate
 )
 
@@ -40,5 +40,5 @@ trainer = Trainer(
 trainer.train()
 
 # Save the fine-tuned model
-model.save_pretrained('fine_tuned_gpt2_model2')
-tokenizer.save_pretrained('fine_tuned_gpt2_model2')
+model.save_pretrained('fine_tuned_gpt2_model')
+tokenizer.save_pretrained('fine_tuned_gpt2_model')
