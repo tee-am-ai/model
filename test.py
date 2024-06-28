495985134d68c16e16edc32581353ec097af15ea
dirgafebrian <fdirga63@gmail.com>
2024-06-28 23:01:36 +0700

    logging test model gpt2

--- test.py
+++ test.py
@@ -1,4 +1,17 @@
 from transformers import GPT2Tokenizer, GPT2LMHeadModel
+import logging
+import os
+
+# Buat folder logs jika belum ada
+if not os.path.exists('log_model'):
+    os.makedirs('log_model')
+
+# Konfigurasi logging
+logging.basicConfig(
+    filename='log_model/generator.log',
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s'
+)
 
 class GPT2Generator:
     def __init__(self, model_path='gpt2'):
@@ -6,7 +19,7 @@ class GPT2Generator:
         self.model = GPT2LMHeadModel.from_pretrained(model_path)
         self.tokenizer = GPT2Tokenizer.from_pretrained(model_path)
 
-    def generate_answer(self, question, max_length=120):
+    def generate_answer(self, question, max_length=100):
         # Encode the input question
         inputs = self.tokenizer.encode(question + self.tokenizer.eos_token, return_tensors='pt')
         # Generate the answer using the model
@@ -21,11 +34,16 @@ class GPT2Generator:
 
 # Example usage:
 def main():
-    generator = GPT2Generator(model_path='fine_tuned_gpt2_model')
-    question = "aku kecewa?"
+    generator = GPT2Generator(model_path='gpt2_model')
+    question = "aku penat dengan kehidupan ini"
     answer = generator.generate_answer(question)
     print(f"Question: {question}")
     print(f"Answer: {answer}")
+
+    logging.info(f"model: {generator.model_path}")
+    logging.info(f"Question: {question}")
+    logging.info(f"Answer: {answer}")
+    logging.info("-------------------\n")
 
 if __name__ == "__main__":
     main()
