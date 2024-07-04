8cddca8e9d35ddd6a23453dc225721433a81ebc5
dirgafebrian <fdirga63@gmail.com>
2024-07-04 22:08:36 +0700

    latih model gpt2

--- train.py
+++ train.py
@@ -4,7 +4,7 @@ from utils import QADataset
 import csv
 
 # Load the dataset
-df = pd.read_csv('datasets/coba.csv', delimiter='|', names=['question', 'answer'], encoding='utf-8', quoting=csv.QUOTE_NONE)
+df = pd.read_csv('datasets/coba.csv', delimiter='|', names=['question', 'answer'], encoding='utf-8', quoting=csv.QUOTE_NONE, on_bad_lines='skip')
 
 # Prepare the dataset
 tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
@@ -20,10 +20,10 @@ model = GPT2LMHeadModel.from_pretrained('gpt2')
 
 # Define training arguments
 training_args = TrainingArguments(
-    output_dir='./results',         
+    output_dir='./results_coba',         
     num_train_epochs=3,              
     per_device_train_batch_size=2,  
-    warmup_steps=500,                
+    warmup_steps=200,                
     weight_decay=0.01,               
     logging_dir='./logs',            
 )
