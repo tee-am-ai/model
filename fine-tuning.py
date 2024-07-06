e086b3cfe217db3aa86e5d0120d384bc1d7fd6b8
dirgafebrian <fdirga63@gmail.com>
2024-07-06 12:20:36 +0700

    Update class QADataset

--- fine-tuning.py
+++ fine-tuning.py
@@ -1,13 +1,20 @@
 import pandas as pd
-from transformers import GPT2Tokenizer, GPT2LMHeadModel, Trainer, TrainingArguments
-import torch
+import csv
+from transformers import GPT2Tokenizer, GPT2LMHeadModel, Trainer, TrainingArguments, DataCollatorForLanguageModeling
 from utils import QADataset
 
 # Load the dataset
-df = pd.read_csv('datasets/clean.csv', delimiter='|', names=['question', 'answer'])
+def filter_valid_rows(row):
+    return len(row) == 2
+
+with open('datasets/clean.csv', 'r', encoding='utf-8') as file:
+    reader = csv.reader(file, delimiter='|')
+    filtered_rows = [row for row in reader if filter_valid_rows(row)]
+
+df = pd.DataFrame(filtered_rows, columns=['question', 'answer'])
 
 # Prepare the dataset
-tokenizer = GPT2Tokenizer.from_pretrained('fine_tuned_gpt2_model')
+tokenizer = GPT2Tokenizer.from_pretrained('gpt2_model')
 tokenizer.pad_token = tokenizer.eos_token
 
 # Combine question and answer into a single string for training
@@ -16,29 +23,38 @@ inputs = df['question'] + tokenizer.eos_token + df['answer']
 dataset = QADataset(inputs, tokenizer)
 
 # Load model
-model = GPT2LMHeadModel.from_pretrained('fine_tuned_gpt2_model')
+model = GPT2LMHeadModel.from_pretrained('gpt2_model')
 
-# Define training arguments for fine-tuning
-fine_tuning_args = TrainingArguments(
-    output_dir='./fine_tuning_results1',
-    num_train_epochs=8,                  # Meningkatkan jumlah epoch
-    per_device_train_batch_size=8,       # Meningkatkan batch size
-    warmup_steps=500,                    # Meningkatkan warmup steps
-    weight_decay=0.01,                   # Menyesuaikan weight decay
-    logging_dir='./fine_tuning_logs1',
-    learning_rate=5e-5,                  # Menyesuaikan learning rate
+# Define data collator
+data_collator = DataCollatorForLanguageModeling(
+    tokenizer=tokenizer,
+    mlm=False,
+)
+
+# Define training arguments
+training_args = TrainingArguments(
+    output_dir='./results',
+    num_train_epochs=3,
+    per_device_train_batch_size=4,
+    warmup_steps=500,
+    weight_decay=0.01,
+    logging_dir='./logs',
+    logging_steps=10,
+    save_steps=500,
+    save_total_limit=2,
 )
 
 # Create Trainer
 trainer = Trainer(
-    model=model,                         
-    args=fine_tuning_args,                  
-    train_dataset=dataset,         
+    model=model,
+    args=training_args,
+    train_dataset=dataset,
+    data_collator=data_collator,
 )
 
-# Fine-tune the model
+# Train the model
 trainer.train()
 
-# Save the fine-tuned model
-model.save_pretrained('fine_tuned_gpt2_model1')
-tokenizer.save_pretrained('fine_tuned_gpt2_model1')
+# Save the model
+model.save_pretrained('fine_tuned_gpt2_model')
+tokenizer.save_pretrained('fine_tuned_gpt2_model')
