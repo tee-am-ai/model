bab16dc72839734d9845c5c874a5b85491561b09
dirgafebrian <fdirga63@gmail.com>
2024-07-07 20:36:34 +0700

    fine tuning model (1)

--- fine-tuning.py
+++ fine-tuning.py
@@ -33,12 +33,12 @@ data_collator = DataCollatorForLanguageModeling(
 
 # Define training arguments
 training_args = TrainingArguments(
-    output_dir='./fine_tuning_results',
+    output_dir='./result/fine_tuning_results1',
     num_train_epochs=5,
-    per_device_train_batch_size=6,
+    per_device_train_batch_size=8,
     warmup_steps=500,
     weight_decay=0.01,
-    logging_dir='./fine_tuning_logs',
+    logging_dir='./fine_tuning_logs1',
     logging_steps=10,
     save_steps=200,
     save_total_limit=2,
@@ -56,5 +56,5 @@ trainer = Trainer(
 trainer.train()
 
 # Save the model
-model.save_pretrained('model/fine_tuned_gpt2_model')
-tokenizer.save_pretrained('model/fine_tuned_gpt2_model')
+model.save_pretrained('model/fine_tuned_gpt2_model1')
+tokenizer.save_pretrained('model/fine_tuned_gpt2_model1')
