fe05118d21eeca2656eaa3ed1756121f8a0bf143
Fatwaff <fax.mp4@gmail.com>
2024-07-13 17:39:03 +0700

    Add GPT2Tokenizer initialization

--- trainer-fatwa/train.py
+++ trainer-fatwa/train.py
@@ -2,3 +2,4 @@ import pandas as pd
 from datasets import Dataset
 from transformers import GPT2LMHeadModel, GPT2Tokenizer, Trainer, TrainingArguments
 
+tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
