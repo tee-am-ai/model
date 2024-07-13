d86b76deef0c6a773538e0883600a6e5d764bb4f
Fatwaff <fax.mp4@gmail.com>
2024-07-13 09:03:05 +0700

    Add tokenizer and model initialization

--- trainer-fatwa/main.py
+++ trainer-fatwa/main.py
@@ -35,3 +35,8 @@ tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 tokenizer.add_special_tokens({"pad_token": "<pad>", 
                                 "bos_token": "<startofstring>",
                                 "eos_token": "<endofstring>"})
+
+tokenizer.add_tokens(["<bot>:"])
+
+model = GPT2LMHeadModel.from_pretrained("gpt2")
+model.resize_token_embeddings(len(tokenizer))
