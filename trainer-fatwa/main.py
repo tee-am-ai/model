f65d86fd55f6a81770e5a6a0347636e5bf5006c8
Fatwaff <fax.mp4@gmail.com>
2024-07-13 09:01:27 +0700

    Add training function to main.py

--- trainer-fatwa/main.py
+++ trainer-fatwa/main.py
@@ -4,3 +4,18 @@ from torch.optim import Adam
 from torch.utils.data import DataLoader
 import tqdm
 import torch
+
+def train(chatData, model, optim):
+
+    epochs = 12
+
+    for i in tqdm.tqdm(range(epochs)):
+        for X, a in chatData:
+            X = X.to(device)
+            a = a.to(device)
+            optim.zero_grad()
+            loss = model(X, attention_mask=a, labels=X).loss
+            loss.backward()
+            optim.step()
+        torch.save(model.state_dict(), "model_state.pt")
+        print(infer("hello how are you"))
