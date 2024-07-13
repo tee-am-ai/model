58d1173093ddc49504b0898a2fb122aff459e382
Fatwaff <fax.mp4@gmail.com>
2024-07-13 09:06:03 +0700

    Add text data extraction from dialog in ChatData class

--- trainer-fatwa/ChatData.py
+++ trainer-fatwa/ChatData.py
@@ -5,3 +5,7 @@ class ChatData(Dataset):
     def __init__(self, path:str, tokenizer):
         self.data = json.load(open(path, "r"))
         
+        self.X = []
+        for i in self.data:
+            for j in i['dialog']:
+                self.X.append(j['text'])
