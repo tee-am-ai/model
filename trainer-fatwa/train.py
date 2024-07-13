7b6ef713df188c295250c0e501ba4554ab2453e6
Fatwaff <fax.mp4@gmail.com>
2024-07-13 17:40:07 +0700

    Add data preprocessing step to tokenize text

--- trainer-fatwa/train.py
+++ trainer-fatwa/train.py
@@ -6,3 +6,7 @@ tokenizer = GPT2Tokenizer.from_pretrained("gpt2")
 
 def tokenize_text(examples):
     return tokenizer(examples['text'], truncation=True, padding="max_length", max_length=512)
+
+df = pd.read_csv("../dialogs.csv", delimiter='|')
+
+df['text'] = df['question'] + " " + df['answer']
