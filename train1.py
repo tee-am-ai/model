215d683cdf0a9f677e8ac31ea7878d2a1b2a8fce
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-17 17:55:31 +0700

    Refactor Trainer initialization in train1.py

--- train1.py
+++ train1.py
@@ -106,14 +106,14 @@ def compute_metrics(eval_pred):
     }
 
 # Trainer
-trainer = Trainer(
-    model=model,
-    args=training_args,
-    train_dataset=dataset_train,
-    eval_dataset=dataset_test,
-    data_collator=data_collator,
-    compute_metrics=compute_metrics
-)
+# trainer = Trainer(
+#     model=model,
+#     args=training_args,
+#     train_dataset=dataset_train,
+#     eval_dataset=dataset_test,
+#     data_collator=data_collator,
+#     compute_metrics=compute_metrics
+# )
 
 # Train the model
 trainer.train()
