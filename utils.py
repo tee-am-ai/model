4c5d909f156e56083e529e0f7e49dd5683538b46
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-07-18 17:43:03 +0700

    Refactor logging configuration in utils.py

--- utils.py
+++ utils.py
@@ -50,12 +50,12 @@ class QADataset(Dataset):
 
 
 # Logging configuration
-# def logging_config(log_dir, log_filename):
-#     if not os.path.exists(log_dir):
-#         os.makedirs(log_dir)
+def logging_config(log_dir, log_filename):
+    if not os.path.exists(log_dir):
+        os.makedirs(log_dir)
 
-#     logging.basicConfig(
-#         filename=f'{log_dir}/{log_filename}',
-#         level=logging.INFO,
-#         format='%(asctime)s - %(levelname)s - %(message)s'
-#     )
+    logging.basicConfig(
+        filename=f'{log_dir}/{log_filename}',
+        level=logging.INFO,
+        format='%(asctime)s - %(levelname)s - %(message)s'
+    )
