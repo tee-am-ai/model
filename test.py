6472d6e5e1ca16a3832881eb369e14309fd40ff6
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-18 22:09:29 +0700

    improve code readability in test.py

--- test.py
+++ test.py
@@ -1,26 +1,29 @@
-import logging
-from utils import logging_config, GPT2Generator
+import logging  # Mengimpor modul logging untuk mencatat log
+from utils import logging_config, GPT2Generator  # Mengimpor logging_config dan GPT2Generator dari modul utils
 
+# Mengkonfigurasi logging dengan nama 'log_model' dan file log 'generator_test.log'
 logging_config('log_model', 'generator_test.log')
 
 def main():
+    # Membuat instance dari GPT2Generator dengan path model yang telah dilatih
     generator = GPT2Generator(model_path='model/gpt2_cobacoba-20-24')
     
-    while True:
-        question = input("Masukkan pertanyaan (atau ketik 'exit' untuk keluar): ")
+    while True:  # Memulai loop tak terbatas untuk menerima input dari pengguna
+        question = input("Masukkan pertanyaan (atau ketik 'exit' untuk keluar): ")  # Menerima input pertanyaan dari pengguna
         
-        if question.lower() == 'exit':
-            print("Terminating the program...")
-            break
+        if question.lower() == 'exit':  # Memeriksa apakah pengguna ingin keluar
+            print("Terminating the program...")  # Mencetak pesan bahwa program akan berhenti
+            break  # Keluar dari loop
         
+        # Menghasilkan jawaban menggunakan metode generate_answer dari GPT2Generator
         answer = generator.generate_answer(question)
-        print(f"Jawaban: {answer}")
+        print(f"Jawaban: {answer}")  # Mencetak jawaban ke layar
 
-        # Log the result
-        logging.info(f"Model: {generator.model_path}")
-        logging.info(f"Pertanyaan: {question}")
-        logging.info(f"Jawaban: {answer}")
-        logging.info("------------------------------------------\n")
+        # Mencatat hasil ke file log
+        logging.info(f"Model: {generator.model_path}")  # Mencatat path model yang digunakan
+        logging.info(f"Pertanyaan: {question}")  # Mencatat pertanyaan dari pengguna
+        logging.info(f"Jawaban: {answer}")  # Mencatat jawaban yang dihasilkan
+        logging.info("------------------------------------------\n")  # Mencatat garis pemisah untuk kejelasan log
 
-if __name__ == "__main__":
-    main()
+if __name__ == "__main__":  # Memastikan bahwa fungsi main() hanya dijalankan jika skrip ini dieksekusi langsung
+    main()  # Menjalankan fungsi utama
