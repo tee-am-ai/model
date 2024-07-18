4d5aba413caebf984ce7db66b36a1c13124f40bc
Novi <alnoviantiramadhani91@gmail.com>
2024-07-18 23:49:20 +0700

    Refactor train.py: Clean up code and optimize dataset loading

--- train.py
+++ train.py
@@ -6,33 +6,25 @@ from utils import QADataset
 # Load the dataset
 def filter_valid_rows(row):
     return len(row) == 2
-
 name = 'clean'
 with open(f'datasets/{name}.csv', 'r', encoding='utf-8') as file:
     reader = csv.reader(file, delimiter='|')
     filtered_rows = [row for row in reader if filter_valid_rows(row)]
-
 df = pd.DataFrame(filtered_rows, columns=['question', 'answer'])
-
 # Prepare the dataset
 model_name = 'gpt2'
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
-
 # Combine question and answer into a single string for training
 inputs = df['question'] + tokenizer.eos_token + df['answer']
-
 dataset = QADataset(inputs, tokenizer, max_length=64)
-
 # Load model
 model = GPT2LMHeadModel.from_pretrained(model_name)
-
 # Define data collator
 data_collator = DataCollatorForLanguageModeling(
     tokenizer=tokenizer,
     mlm=False,
 )
-
 # Define training arguments
 training_args = TrainingArguments(
     output_dir=f'./result/results_{name}',
@@ -45,7 +37,6 @@ training_args = TrainingArguments(
     save_steps=500,
     save_total_limit=2,
 )
-
 # Create Trainer
 trainer = Trainer(
     model=model,
@@ -53,10 +44,8 @@ trainer = Trainer(
     train_dataset=dataset,
     data_collator=data_collator,
 )
-
 # Train the model
 trainer.train()
-
 # Save the model
 model_path = f'model/gpt2_model_{name}'
 model.save_pretrained(model_path)
