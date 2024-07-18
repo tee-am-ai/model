632d7d4d86fa5c365ae0b8edfe31c6211f2f9eb7
Novi <alnoviantiramadhani91@gmail.com>
2024-07-19 00:18:07 +0700

    Split dataset

--- train1.py
+++ train1.py
@@ -25,8 +25,8 @@ with open(f'datasets/{num}.csv', 'r', encoding='utf-8') as file:
 
 df = pd.DataFrame(filtered_rows, columns=['question', 'answer'])
 
-# # Split dataset into training and test sets
-# train_df, test_df = tts(df, test_size=0.2, random_state=42)
+# Split dataset into training and test sets
+train_df, test_df = tts(df, test_size=0.2, random_state=42)
 
 # # Reset index to ensure continuous indexing
 # train_df = train_df.reset_index(drop=True)
