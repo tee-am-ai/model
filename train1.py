c4617e5604c4dc60f419ad857ae2c51bd5f073c4
Novi <alnoviantiramadhani91@gmail.com>
2024-07-19 00:35:06 +0700

    Clean code on train1.py

--- train1.py
+++ train1.py
@@ -13,6 +13,7 @@ logging_config('log_model', 'generator_accuracy.log')
 # Function to filter valid rows
 def filter_valid_rows(row):
     return len(row) == 2 and all(row)
+
 # Load the dataset
 num = 'coba'
 filtered_rows = []
@@ -21,28 +22,37 @@ with open(f'datasets/{num}.csv', 'r', encoding='utf-8') as file:
     for row in reader:
         if filter_valid_rows(row):
             filtered_rows.append(row)
+
 df = pd.DataFrame(filtered_rows, columns=['question', 'answer'])
+
 # Split dataset into training and test sets
 train_df, test_df = tts(df, test_size=0.2, random_state=42)
+
 # Reset index to ensure continuous indexing
 train_df = train_df.reset_index(drop=True)
 test_df = test_df.reset_index(drop=True)
+
 # Prepare the dataset
 model_name = 'gpt2'
 tokenizer = GPT2Tokenizer.from_pretrained(model_name)
 tokenizer.pad_token = tokenizer.eos_token
+
 # Combine question and answer into a single string for training
 inputs_train = train_df['question'] + tokenizer.eos_token + train_df['answer']
 dataset_train = QADataset(inputs_train, tokenizer, max_length=64)
+
 inputs_test = test_df['question'] + tokenizer.eos_token + test_df['answer']
 dataset_test = QADataset(inputs_test, tokenizer, max_length=64)
+
 # Load model
 model = GPT2LMHeadModel.from_pretrained(model_name)
+
 # Define data collator
 data_collator = DataCollatorForLanguageModeling(
     tokenizer=tokenizer,
     mlm=False,
 )
+
 epoch = 20
 batch_size = 24
 # Define training arguments
@@ -62,10 +72,12 @@ training_args = TrainingArguments(
     # eval_strategy="epoch",
     # eval_steps=500,
 )
+
 # Load metrics
 # accuracy_metric = evaluate.load("accuracy", trust_remote_code=True)
 bleu_metric = evaluate.load("bleu", trust_remote_code=True)
 # rouge_metric = evaluate.load("rouge", trust_remote_code=True)
+
 def compute_metrics(eval_pred):
     logits, labels = eval_pred
     if isinstance(logits, np.ndarray):
@@ -83,14 +95,17 @@ def compute_metrics(eval_pred):
     mask = labels != -100
     predictions = predictions[mask]
     labels = labels[mask]
+
     # accuracy = accuracy_metric.compute(predictions=predictions, references=labels)
     bleu = bleu_metric.compute(predictions=predictions, references=labels)
     # rouge = rouge_metric.compute(predictions=predictions, references=labels)
+
     return {
         # "accuracy": accuracy,
         "bleu": bleu,
         # "rouge": rouge,
     }
+
 # Trainer
 trainer = Trainer(
     model=model,
@@ -100,14 +115,18 @@ trainer = Trainer(
     data_collator=data_collator,
     compute_metrics=compute_metrics
 )
+
 # Train the model
 trainer.train()
+
 # Save the model
 path = f'model/gpt2_coba{num}-{epoch}-{batch_size}'
 model.save_pretrained(path)
 tokenizer.save_pretrained(path)
+
 # Evaluate model
 eval_results = trainer.evaluate()
+
 # Print evaluation results, including accuracy
 print(f"Evaluation results: {eval_results}")
 logging.info(f"Model: {path}")
