35e2ee68131c73777f51d5a5ba1ab663101f806f
Novi <alnoviantiramadhani91@gmail.com>
2024-07-18 23:53:39 +0700

    Refactor GPT2Generator class on generate_answer func

--- utils.py
+++ utils.py
@@ -11,15 +11,15 @@ class GPT2Generator:
         self.model = GPT2LMHeadModel.from_pretrained(model_path)
         self.tokenizer = GPT2Tokenizer.from_pretrained(model_path)
 
-    # def generate_answer(self, question, max_length):
-    #     inputs = self.tokenizer.encode(question + self.tokenizer.eos_token, return_tensors='pt')
-    #     outputs = self.model.generate(inputs, max_length=max_length, pad_token_id=self.tokenizer.eos_token_id)
-    #     answer = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
+    def generate_answer(self, question, max_length):
+        inputs = self.tokenizer.encode(question + self.tokenizer.eos_token, return_tensors='pt')
+        outputs = self.model.generate(inputs, max_length=max_length, pad_token_id=self.tokenizer.eos_token_id)
+        answer = self.tokenizer.decode(outputs[0], skip_special_tokens=True)
 
-    #     if answer.startswith(question):
-    #         answer = answer[len(question):].strip()
+        if answer.startswith(question):
+            answer = answer[len(question):].strip()
 
-    #     return answer
+        return answer
     
     # def calculate_bleu_score(self, reference, generated):
     #     reference_tokens = self.tokenizer.tokenize(reference)
