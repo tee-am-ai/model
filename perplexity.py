f4c48e66b6259892fc7e142b50325fba69ea2448
Novi <alnoviantiramadhani91@gmail.com>
2024-07-19 14:55:17 +0700

    Update calculate_perplexity on perplexity.py

--- perplexity.py
+++ perplexity.py
@@ -29,3 +29,30 @@ def prepare_model(model_path):
     model.eval()
     return model
 
+def calculate_perplexity(model, dataset, batch_size=6):
+    data_loader = DataLoader(dataset, batch_size=batch_size)
+    total_loss = 0.0
+    for batch in data_loader:
+        input_ids = batch['input_ids']
+        attention_mask = batch['attention_mask']
+        with torch.no_grad():
+            outputs = model(input_ids, attention_mask=attention_mask, labels=input_ids)
+            total_loss += outputs.loss.item()
+    avg_loss = total_loss / len(data_loader)
+    return torch.exp(torch.tensor(avg_loss)).item()
+
+# def main():
+#     logging_config('log_model', 'generator_perplexity.log')
+#     df = load_dataset('datasets/clean.csv')
+#     tokenizer = prepare_tokenizer('model/fine_tuned_gpt2_model2')
+#     inputs = combine_questions_answers(df, tokenizer)
+#     dataset = QADataset(inputs, tokenizer, max_length=64)
+#     model = prepare_model('model/fine_tuned_gpt2_model2')
+#     perplexity = calculate_perplexity(model, dataset)
+#     logging.info(f"Model: model/fine_tuned_gpt2_model2")
+#     logging.info(f'Perplexity: {perplexity}')
+#     logging.info("------------------------------------------\n")
+#     print(f'Perplexity: {perplexity}')
+
+# if __name__ == "__main__":
+#     main()
