06d63688c6eb3a87cf689df5374e027aabc30546
Novi <alnoviantiramadhani91@gmail.com>
2024-07-19 14:22:31 +0700

    Update imports in test.py

--- test.py
+++ test.py
@@ -3,24 +3,24 @@ from utils import logging_config, GPT2Generator
 
 logging_config('log_model', 'generator_test.log')
 
-def main():
-    generator = GPT2Generator(model_path='model/gpt2_cobacoba-20-24')
+# def main():
+#     generator = GPT2Generator(model_path='model/gpt2_cobacoba-20-24')
     
-    while True:
-        question = input("Masukkan pertanyaan (atau ketik 'exit' untuk keluar): ").strip()
+#     while True:
+#         question = input("Masukkan pertanyaan (atau ketik 'exit' untuk keluar): ").strip()
         
-        if question.lower() == 'exit':
-            print("Terminating the program...")
-            break
+#         if question.lower() == 'exit':
+#             print("Terminating the program...")
+#             break
         
-        answer = generator.generate_answer(question, max_length=100)
-        print(f"Jawaban: {answer}")
+#         answer = generator.generate_answer(question, max_length=100)
+#         print(f"Jawaban: {answer}")
 
-        # Log the result
-        logging.info(f"Model: {generator.model_path}")
-        logging.info(f"Pertanyaan: {question}")
-        logging.info(f"Jawaban: {answer}")
-        logging.info("------------------------------------------\n")
+#         # Log the result
+#         logging.info(f"Model: {generator.model_path}")
+#         logging.info(f"Pertanyaan: {question}")
+#         logging.info(f"Jawaban: {answer}")
+#         logging.info("------------------------------------------\n")
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
