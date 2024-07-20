c5da3c6634e2ea15d8fe3457c19c5a0191cf7275
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-20 16:45:27 +0700

    update fix code belu.py

--- bleu.py
+++ bleu.py
@@ -1,14 +1,15 @@
-import logging  # Mengimpor modul logging untuk mencatat log
-from utils import logging_config, GPT2Generator  # Mengimpor fungsi logging_config dan kelas GPT2Generator dari modul utils
+import logging
+from utils import logging_config, GPT2Generator
 
-# Konfigurasi logging
-logging.basicConfig(filename='generator_bleu.log', level=logging.INFO)
+logging_config('log_model', 'generator_bleu.log')
 
-# Fungsi utama
+# Example usage:
 def main():
-    generator = GPT2Generator(model_path='gpt2_model_coba')  # Membuat instance generator GPT-2 dengan path model yang ditentukan
-    example_question = "ibukota indonesia apa?"  # Pertanyaan contoh
-    reference_answer = "jakarta"  # Jawaban referensi
+    generator = GPT2Generator(model_path='gpt2_model_coba')
+
+    # Example usage to generate and evaluate BLEU score
+    example_question = "ibukota indonesia apa?"
+    reference_answer = "jakarta"
 
     generated_answer = generator.generate_answer(example_question, max_length=100)
     print(f"Question: {example_question}")
@@ -17,7 +18,6 @@ def main():
     bleu_score = generator.calculate_bleu_score(reference_answer, generated_answer)
     print("BLEU Score:", bleu_score)
 
-    # Mencatat informasi ke dalam log
     logging.info(f"Model: {generator.model_path}")
     logging.info(f"Question: {example_question}")
     logging.info(f"Answer: {generated_answer}")
@@ -25,6 +25,5 @@ def main():
     logging.info(f"BLEU Score: {bleu_score}")
     logging.info("------------------------------------------\n")
 
-# Memastikan fungsi main() berjalan jika skrip dieksekusi secara langsung
 if __name__ == "__main__":
     main()
