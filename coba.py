bf3677b41342042b3b41287172cb8449426144e0
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-20 16:24:23 +0700

    ubah kode

--- coba.py
+++ coba.py
@@ -4,27 +4,29 @@ from utils import logging_config, GPT2Generator  # Mengimpor fungsi logging_conf
 # Kode ini mengimpor modul logging dan dua komponen dari modul utils,
 # yaitu fungsi logging_config untuk konfigurasi logging dan kelas GPT2Generator untuk menghasilkan teks menggunakan model GPT-2.
 
+# Konfigurasi logging
+logging.basicConfig(filename='generator_test.log', level=logging.INFO)
 
-logging_config('log_model', 'generator_test.log')
-
+# Fungsi utama
 def main():
-    generator = GPT2Generator(model_path='model/gpt2_cobacoba-20-24')
+    generator = GPT2Generator(model_path='model/gpt2_cobacoba-20-24')  # Membuat instance generator GPT-2 dengan path model yang ditentukan
     
-    while True:
-        question = input("Masukkan pertanyaan (atau ketik 'exit' untuk keluar): ").strip()
+    while True:  # Memulai loop untuk menerima input dari pengguna
+        question = input("Masukkan pertanyaan (atau ketik 'exit' untuk keluar): ").strip()  # Meminta input pertanyaan dari pengguna
         
-        if question.lower() == 'exit':
+        if question.lower() == 'exit':  # Memeriksa apakah pengguna ingin keluar
             print("Terminating the program...")
-            break
+            break  # Menghentikan loop dan keluar dari program
         
-        answer = generator.generate_answer(question, max_length=100)
-        print(f"Jawaban: {answer}")
+        answer = generator.generate_answer(question, max_length=100)  # Menghasilkan jawaban menggunakan generator GPT-2
+        print(f"Jawaban: {answer}")  # Mencetak jawaban yang dihasilkan
 
-        # Log the result
+        # Mencatat hasil ke dalam log
         logging.info(f"Model: {generator.model_path}")
         logging.info(f"Pertanyaan: {question}")
         logging.info(f"Jawaban: {answer}")
         logging.info("------------------------------------------\n")
 
+# Memastikan fungsi main() berjalan jika skrip dieksekusi secara langsung
 if __name__ == "__main__":
     main()
