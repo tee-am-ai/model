5b7d29fa49e718fb8302ca04844c2c15045a81f0
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-20 16:17:15 +0700

    Refactor code to remove unused imports and improve performance and readability

--- train.py
+++ train.py
@@ -7,10 +7,10 @@ from utils import QADataset
 def filter_valid_rows(row):
     return len(row) == 2
 
-name = 'clean'
-with open(f'datasets/{name}.csv', 'r', encoding='utf-8') as file:
-    reader = csv.reader(file, delimiter='|')
-    filtered_rows = [row for row in reader if filter_valid_rows(row)]
+# name = 'clean'
+# with open(f'datasets/{name}.csv', 'r', encoding='utf-8') as file:
+#     reader = csv.reader(file, delimiter='|')
+#     filtered_rows = [row for row in reader if filter_valid_rows(row)]
 
 df = pd.DataFrame(filtered_rows, columns=['question', 'answer'])
 
