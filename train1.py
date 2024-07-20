f614a43029b4c1da82b61ca98dbb83edab9d577c
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-20 16:31:55 +0700

    Refactor code to enable training and testing datasets

--- train1.py
+++ train1.py
@@ -106,15 +106,15 @@ def compute_metrics(eval_pred):
         # "rouge": rouge,
     }
 
-# # Trainer
-# trainer = Trainer(
-#     model=model,
-#     args=training_args,
-#     train_dataset=dataset_train,
-#     eval_dataset=dataset_test,
-#     data_collator=data_collator,
-#     compute_metrics=compute_metrics
-# )
+# Trainer
+trainer = Trainer(
+    model=model,
+    args=training_args,
+    train_dataset=dataset_train,
+    eval_dataset=dataset_test,
+    data_collator=data_collator,
+    compute_metrics=compute_metrics
+)
 
 # Train the model
 trainer.train()
