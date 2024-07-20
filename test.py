464f2508c4ed46446baadb8e58a19abc0ea15e78
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-20 16:46:21 +0700

    ubah code

--- test.py
+++ test.py
@@ -1,8 +1,7 @@
-import logging  # Mengimpor modul logging untuk mencatat log
-from utils import logging_config, GPT2Generator  # Mengimpor fungsi logging_config dan kelas GPT2Generator dari modul utils
+import logging
+from utils import logging_config, GPT2Generator
 
-# Mengkonfigurasi logging
-logging.basicConfig(filename='generator_test.log', level=logging.INFO)
+logging_config('log_model', 'generator_test.log')
 
 def main():
     generator = GPT2Generator(model_path='model/gpt2_cobacoba-20-24')
@@ -11,14 +10,13 @@ def main():
         question = input("Masukkan pertanyaan (atau ketik 'exit' untuk keluar): ").strip()
         
         if question.lower() == 'exit':
-            # Menghentikan program jika pengguna mengetik 'exit'
             print("Terminating the program...")
             break
         
         answer = generator.generate_answer(question, max_length=100)
         print(f"Jawaban: {answer}")
 
-        # Mencatat hasil ke file log
+        # Log the result
         logging.info(f"Model: {generator.model_path}")
         logging.info(f"Pertanyaan: {question}")
         logging.info(f"Jawaban: {answer}")
