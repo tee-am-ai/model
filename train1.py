051d4d9ee3f9ca6dfb7cbbab9d22ce78e68f4a81
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-20 16:31:41 +0700

    Refactor compute_metrics function for better performance and readability

--- train1.py
+++ train1.py
@@ -78,14 +78,14 @@ training_args = TrainingArguments(
 bleu_metric = evaluate.load("bleu", trust_remote_code=True)
 # rouge_metric = evaluate.load("rouge", trust_remote_code=True)
 
-# def compute_metrics(eval_pred):
-#     logits, labels = eval_pred
-#     if isinstance(logits, np.ndarray):
-#         logits = torch.tensor(logits)
-#     if isinstance(labels, np.ndarray):
-#         labels = torch.tensor(labels)
+def compute_metrics(eval_pred):
+    logits, labels = eval_pred
+    if isinstance(logits, np.ndarray):
+        logits = torch.tensor(logits)
+    if isinstance(labels, np.ndarray):
+        labels = torch.tensor(labels)
     
-    # predictions = torch.argmax(logits, dim=-1)
+    predictions = torch.argmax(logits, dim=-1)
     
     # Flatten tensors to 1D
     predictions = predictions.view(-1)
