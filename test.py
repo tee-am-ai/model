91e95ae6e926d165dba491d0df267f86e6def6c4
Fedhira Syaila <pedirasyaila@gmail.com>
2024-07-20 16:04:29 +0700

    Refactor logging and user input handling in main function

--- test.py
+++ test.py
@@ -1,29 +1,26 @@
 import logging
 from utils import logging_config, GPT2Generator
 
-# Mengkonfigurasi logging dengan nama 'log_model' dan file log 'generator_test.log'
 logging_config('log_model', 'generator_test.log')
 
 def main():
-    # Membuat instance dari GPT2Generator dengan path model yang telah dilatih
     generator = GPT2Generator(model_path='model/gpt2_cobacoba-20-24')
     
-    while True:  # Memulai loop tak terbatas untuk menerima input dari pengguna
-        question = input("Masukkan pertanyaan (atau ketik 'exit' untuk keluar): ")  # Menerima input pertanyaan dari pengguna
+    while True:
+        question = input("Masukkan pertanyaan (atau ketik 'exit' untuk keluar): ").strip()
         
-        if question.lower() == 'exit':  # Memeriksa apakah pengguna ingin keluar
-            print("Terminating the program...")  # Mencetak pesan bahwa program akan berhenti
-            break  # Keluar dari loop
+        if question.lower() == 'exit':
+            print("Terminating the program...")
+            break
         
-        # Menghasilkan jawaban menggunakan metode generate_answer dari GPT2Generator
-        answer = generator.generate_answer(question)
-        print(f"Jawaban: {answer}")  # Mencetak jawaban ke layar
+        answer = generator.generate_answer(question, max_length=100)
+        print(f"Jawaban: {answer}")
 
-        # Mencatat hasil ke file log
-        logging.info(f"Model: {generator.model_path}")  # Mencatat path model yang digunakan
-        logging.info(f"Pertanyaan: {question}")  # Mencatat pertanyaan dari pengguna
-        logging.info(f"Jawaban: {answer}")  # Mencatat jawaban yang dihasilkan
-        logging.info("------------------------------------------\n")  # Mencatat garis pemisah untuk kejelasan log
+        # Log the result
+        logging.info(f"Model: {generator.model_path}")
+        logging.info(f"Pertanyaan: {question}")
+        logging.info(f"Jawaban: {answer}")
+        logging.info("------------------------------------------\n")
 
-if __name__ == "__main__":  # Memastikan bahwa fungsi main() hanya dijalankan jika skrip ini dieksekusi langsung
-    main()  # Menjalankan fungsi utama
+if __name__ == "__main__":
+    main()
