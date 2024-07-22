432c0ca4c6188e696ec484c5a8abc0fecbf3fed6
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-07-22 12:29:24 +0700

    update train.py

--- train.py
+++ train.py
@@ -3,9 +3,7 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel, Trainer, TrainingArgume
 import csv
 from utils import QADataset
 
-# Load the dataset
-def filter_valid_rows(row):
-    return len(row) == 2
+
 
 name = 'clean'
 with open(f'datasets/{name}.csv', 'r', encoding='utf-8') as file:
