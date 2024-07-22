a1c543c7405c6aa0dd749e0c0eecf3789e27c8e8
RizkyriaHutabarat <rizkyriahutabarat18@gmail.com>
2024-07-22 12:30:24 +0700

    Refactor train.py to load dataset and filter valid rows

--- train.py
+++ train.py
@@ -3,7 +3,9 @@ from transformers import GPT2Tokenizer, GPT2LMHeadModel, Trainer, TrainingArgume
 import csv
 from utils import QADataset
 
-
+# Load the dataset
+def filter_valid_rows(row):
+    return len(row) == 2
 
 name = 'clean'
 with open(f'datasets/{name}.csv', 'r', encoding='utf-8') as file:
