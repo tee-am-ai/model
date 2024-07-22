14b419dc21926e0c470fcdd557c4cc225cedb188
ArykaAnisaP <arykaanisap22@gmail.com>
2024-07-22 18:18:58 +0700

    Refactor training arguments in fine-tuning.py

--- fine-tuning.py
+++ fine-tuning.py
@@ -35,19 +35,15 @@ data_collator = DataCollatorForLanguageModeling(
 
 # Define training arguments
 training_args = TrainingArguments(
-    output_dir='./result/fine_tuning_results2',  # Directory to save the model and checkpoints
-    num_train_epochs=10,  # Number of training epochs
-    per_device_train_batch_size=8,  # Batch size per device (GPU/CPU)
-    warmup_steps=500,  # Number of warmup steps for learning rate scheduler
-    weight_decay=0.01,  # Weight decay for regularization
-    logging_dir='./fine_tuning_logs2',  # Directory to save the logs
-    logging_steps=10,  # Number of steps between logging
-    save_steps=200,  # Number of steps between saving checkpoints
-    save_total_limit=2,  # Maximum number of checkpoints to keep
-    evaluation_strategy='steps',  # Strategy for evaluation (e.g., 'steps' or 'epoch')
-    eval_steps=200,  # Number of steps between evaluations (if evaluation_strategy='steps')
-    load_best_model_at_end=True,  # Load the best model when finished training
-    metric_for_best_model='eval_loss',  # Metric to use for selecting the best model
+    output_dir='./result/fine_tuning_results2',
+    num_train_epochs=10,
+    per_device_train_batch_size=8,
+    warmup_steps=500,
+    weight_decay=0.01,
+    logging_dir='./fine_tuning_logs2',
+    logging_steps=10,
+    save_steps=200,
+    save_total_limit=2,
 )
 
 # Create Trainer
