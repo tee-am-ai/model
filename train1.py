20ed661f1df38a05fa82399486271b9e0428b9f4
aulianafahrian <aulianafahrian@gmail.com>
2024-07-27 23:33:32 +0700

    Refactor train1.py and remove unnecessary code

--- train1.py
+++ train1.py
@@ -123,7 +123,3 @@ trainer.train()
 path = f'model/gpt2_coba{num}-{epoch}-{batch_size}'
 model.save_pretrained(path)
 tokenizer.save_pretrained(path)
-
-# Evaluate model
-eval_results = trainer.evaluate()
-
