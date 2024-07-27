9d376dc38ad46ce28307c3fa9d6904b4cf2f56fe
aulianafahrian <aulianafahrian@gmail.com>
2024-07-28 00:00:28 +0700

    Refactor train.py and remove unnecessary code

--- train.py
+++ train.py
@@ -33,4 +33,16 @@ data_collator = DataCollatorForLanguageModeling(
     mlm=False,
 )
 
+# Define training arguments
+training_args = TrainingArguments(
+    output_dir=f'./result/results_{name}',
+    num_train_epochs=3,
+    per_device_train_batch_size=4,
+    warmup_steps=500,
+    weight_decay=0.01,
+    logging_dir='./logs',
+    logging_steps=10,
+    save_steps=500,
+    save_total_limit=2,
+)
 
