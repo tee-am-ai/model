d0824713855738800500064f543641b37b61cf0d
aulianafahrian <aulianafahrian@gmail.com>
2024-07-28 00:11:05 +0700

    Refactor train.py and add chat.py

--- train/chat.py
+++ train/chat.py
@@ -6,10 +6,14 @@ tokenizer = GPT2Tokenizer.from_pretrained(model_path)
 model = GPT2LMHeadModel.from_pretrained(model_path)
 
 # Fungsi untuk menghasilkan teks
+
+
 def generate_text(prompt, max_length=50):
     inputs = tokenizer.encode(prompt, return_tensors="pt")
-    outputs = model.generate(inputs, max_length=max_length, num_return_sequences=1, no_repeat_ngram_size=2, early_stopping=True)
+    outputs = model.generate(inputs, max_length=max_length,
+                             num_return_sequences=1, no_repeat_ngram_size=2, early_stopping=True)
     return tokenizer.decode(outputs[0], skip_special_tokens=True)
+
 
 # Contoh pengujian
 prompt = "Hari ini saya merasa sangat bahagia karena"
