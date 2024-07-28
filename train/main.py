457ac833aa35c8c912b71592ad184b9353ece7fb
aulianafahrian <aulianafahrian@gmail.com>
2024-07-28 11:09:16 +0700

    Refactor train.py and add chat.py

--- train/main.py
+++ train/main.py
@@ -4,3 +4,22 @@ from torch.optim import Adam
 from torch.utils.data import DataLoader
 import tqdm
 import torch
+
+def train(chatData, model, optim):
+
+    epochs = 12
+
+    for i in tqdm.tqdm(range(epochs)):
+        c = 1
+        for X, a in chatData:
+            X = X.to(device)
+            a = a.to(device)
+            optim.zero_grad()
+            loss = model(X, attention_mask=a, labels=X).loss
+            loss.backward()
+            optim.step()
+            print(f"epoch {i} batch {c} loss : {loss.item()}")
+            c+=1
+        torch.save(model.state_dict(), "model_state.pt")
+        print("model saved")
+        print(infer("hello how are you"))
