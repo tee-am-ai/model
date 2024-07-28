1c52bc3306cf3cde517db70d8202e49545f1dbe0
aulianafahrian <aulianafahrian@gmail.com>
2024-07-28 11:08:16 +0700

    chore: Add main.py file

---
+++ train/main.py
@@ -0,0 +1,6 @@
+from transformers import GPT2LMHeadModel, GPT2Tokenizer
+from ChatData import ChatData
+from torch.optim import Adam
+from torch.utils.data import DataLoader
+import tqdm
+import torch
